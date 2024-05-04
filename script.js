6776326babead7a552e7801a9480acb07f2be8cd
Yutao Xue <164786071+ytx-26@users.noreply.github.com>
2024-05-04 14:17:34 +0100

    Adding event listener to button

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+//Input Box
 const userInput = document.getElementById('Input-Box');
 const userInputValue = userInput.value;
 
+
+//Button 
+const Button = document.getElementById('Button');
+
+
 const input = document.getElementById('Input'); 
+
+
+// Getting user input after they click on the button
+Button.addEventListener('click', additionToPrintBox);
+
+
+
